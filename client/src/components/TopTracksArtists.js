e76952ad76315393e6c6bd2d3d2297564361cbe3
Omar Assouma <assouma@sabanciuniv.edu>
2023-01-31 23:05:07 +0200

    finished profile

--- client/src/components/TopTracksArtists.js
+++ client/src/components/TopTracksArtists.js
@@ -3,18 +3,21 @@ import '../sass/TopTracksArtists.scss'
 
 const TopTracksArtists = ({ topArtists, topTracks }) => {
 
-    console.log(topArtists);
+    console.log(topTracks);
 
 
     return (
         topArtists && topTracks && (
             <div className="top-container">
                 <div className="wrapper">
-                    <span className='title'>Top Artists Of All time</span>
                     <div className="mapping-wrapper">
+                        <div className="intro">
+                            <span className='title'>Top Artists Of All time</span>
+                            <a href='/topArtists'><button>See More</button></a>
+                        </div>
                         {topArtists.map(artist => (
                             <div className="artists">
-                                <img src={artist.images[2].url} />
+                                <img src={artist.images[2].url} alt='artist'/>
                                 <span >{artist.name}</span>
                             </div>
                         ))}
@@ -22,10 +25,19 @@ const TopTracksArtists = ({ topArtists, topTracks }) => {
                     </div>
                 </div>
                 <div className="wrapper">
-                    <span className='title'>Top Tracks Of All Time</span>
                     <div className="mapping-wrapper">
+                        <div className="intro">
+                            <span className='title'>Top Tracks Of All Time</span>
+                            <button>See More</button>
+                        </div>
                         {topTracks.map(tracks => (
-                            <span >{tracks.name}</span>
+                            <div className="tracks">
+                                <img src={tracks.album.images[2].url} alt='cover'/>
+                                <div className="track-artist-name">
+                                    <span className='track-name'>{tracks.name}</span>
+                                    <span className='artist-name' >{tracks.artists[0].name}</span>
+                                </div>
+                            </div>
                         ))}
                     </div>
                 </div>
