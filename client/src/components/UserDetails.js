e62362d29da8e6c4da56341b6ea28d7279837ed9
Omar Assouma <assouma@sabanciuniv.edu>
2023-02-19 13:33:05 +0300

    fixed no image bug

--- client/src/components/UserDetails.js
+++ client/src/components/UserDetails.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import '../sass/UserDetails.scss'
 import { Logout } from '../hooks/index'
+import { HiOutlineUserCircle } from "react-icons/hi2";
+
 
 
 const UserDetails = ({ user, following, playlists }) => {
 
     return (
         <div className="avatar">
-            <img src={user.images[0].url} alt='user_picture' />
+            {
+                user.images[0] ? (
+                    <img src={user.images[0].url} alt='user_picture' />
+                ) : <HiOutlineUserCircle className='noImageIcon'/>
+            }
             <div className="information-wrapper">
                 <span className='username'>{user.display_name}</span>
                 <div className="user-information">
